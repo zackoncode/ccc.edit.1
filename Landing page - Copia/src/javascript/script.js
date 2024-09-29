267f9276c196b93fcd8dc40c40af8463710adf92
Zack <151975176+zackoncode@users.noreply.github.com>
2024-09-29 20:37:59 -0300

    Update script.js

--- Landing page - Copia/src/javascript/script.js
+++ Landing page - Copia/src/javascript/script.js
@@ -1,6 +1,4 @@
-//ESTOU TENTANDO COLOCAR A FUNÇÃO DO BOTÃO "FA-BARS" E ELE CARALHOS NÃO QUER FUNCIONAR
-//FALAR COM O PROFESSOR SOBRE O ABSURDO DESSA BOTÃO FDP.
-//DEIXA PRA LA EU CONSEGUI.
+
 $(document).ready(function() {
     $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
